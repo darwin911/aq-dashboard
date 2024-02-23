6d787d6197892d1f6aef7de83c368b006e3828d8
Darwin Smith <darwinpsmith@gmail.com>
2024-02-23 10:01:49 -0500

    chore: update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,9 @@
 import { NextResponse } from "next/server";
-import IPinfoWrapper, { IPinfo, AsnResponse } from "node-ipinfo";
-import { NextRequest } from "next/server";
 
-const token = process.env.IPINFO_TOKEN;
-const ipinfoWrapper = new IPinfoWrapper(token);
+import { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
   console.log("\n** Middleware Running **\n");
-
-  const ip = request.ip || request.headers.get("x-forwarded-for");
-  if (ip) {
-    const ipInfo: IPinfo = await ipinfoWrapper.lookupIp(ip);
-
-    console.log(ipInfo);
-  }
 }
 
 export const config = {
