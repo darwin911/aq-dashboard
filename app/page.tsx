aa4fce58b9e5f46a13913d192cf0b0e19d53ccb4
Darwin Smith <darwinpsmith@gmail.com>
2024-02-25 11:24:09 -0500

    feat: add redrect with geo search params based from ip

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import ClientGeoAQDAta from "@/app/_components/client-geo-aq-data";
 
-export default async function Page() {
+export default async function Page({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  console.log({ searchParams });
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 tracking-tighter">
       <h1 className="text-4xl font-bold mb-5">Air Quality Dashboard</h1>
-      {/* <ClientGeoAQDAta /> */}
     </main>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,12 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
+  if (request.url.includes("?")) {
+    return NextResponse.next();
+  }
+
   let ip = request.headers.get("x-forwarded-for");
-  let geo: geoDataType = null;
+  let geo: any = null;
 
   if (ip && ip.split(",")[0] !== "::1") {
     ip = ip.split(",")[0];
@@ -22,12 +26,44 @@ export async function middleware(request: NextRequest) {
 
     const data = await res.json();
     console.log("\nGeo:", data, "\n");
-    geo = data;
+    geo = new URLSearchParams(data);
   }
 
   console.log("\nGeo", geo, "\n");
 
-  return NextResponse.json({ message: "middleware running", geo, ip });
+  //   const TEMP = new URLSearchParams({
+  //     city: "philly",
+  //     country: "Armenia",
+  //     lat: "2.233",
+  //     lon: "-1.233",
+  //   });
+
+  if (!geo) {
+    console.log("No Geo data found");
+    return NextResponse.next();
+  }
+
+  const example = {
+    ip: "212.102.51.243",
+    hostname: "unn-212-102-51-243.cdn77.com",
+    city: "Tokyo",
+    region: "Tokyo",
+    country: "JP",
+    loc: "35.6895,139.6917",
+    org: "AS212238 Datacamp Limited",
+    postal: "101-8656",
+    timezone: "Asia/Tokyo",
+    readme: "https://ipinfo.io/missingauth",
+  };
+
+  console.log(
+    "Redirecting!",
+    new URL(`/?${geo.toString()}}`, request.url).toString()
+  );
+
+  return NextResponse.redirect(
+    new URL(`/?${geo.toString()}`, request.url).toString()
+  );
 }
 
 export const config = {
