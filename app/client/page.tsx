8d78d0636ddc76f87a5e26875393b164d95a5530
Darwin Smith <darwinpsmith@gmail.com>
2024-02-29 08:18:49 -0500

    feat: add back button

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -1,4 +1,5 @@
 import ClientGeoAQDAta from "@/app/_components/client-geo-aq-data";
+import Link from "next/link";
 
 export default function ClientAQI() {
   return (
@@ -6,6 +7,11 @@ export default function ClientAQI() {
       <h1 className="text-2xl md:text-4xl font-bold mb-5">
         Air Quality Dashboard
       </h1>
+      <div className="container">
+        <Link href="/" className="underline">
+          Back
+        </Link>
+      </div>
       <ClientGeoAQDAta />
     </>
   );
